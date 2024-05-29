45a47fc55faae92f91d0cd73bf52cfdc5556d739
Cesar Trujillo <catgx55@gmail.com>
2024-05-29 16:16:25 -0500

    creacion context y reducer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useContext } from "react"
+import BudgetForm from "./components/BudgetForm"
+import { BudgetContext } from "./context/BudgetContext"
+
 function App() {
+
+  const context = useContext(BudgetContext)
+  console.log(context)
 
   return (
     <>
-      <h1>Control de gastos</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm />
+      </div>
     </>
   )
 }
---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, ReactNode, createContext, useReducer } from "react";
+import { BudgetActions, BudgetState, budgetReducer, initialState } from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+    state: BudgetState
+    dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider = ({children} : BudgetProviderProps) => {
+
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    )
+}
